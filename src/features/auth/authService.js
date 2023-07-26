5585fcaefbcee999b63fd96a2315394aaa7d396d
Marco Monster <marco.disegnio@gmail.com>
2023-07-25 19:33:18 -0600

    se creo la funcion de logout

--- src/features/auth/authService.js
+++ src/features/auth/authService.js
@@ -17,9 +17,14 @@ const login = async (userData) => {
     return response.data;
  
 }
+
+const logout = () => {
+    localStorage.removeItem('user')
+}
 const authService  = {
     register,
-    login
+    login,
+    logout
 }
 
 export default authService;
